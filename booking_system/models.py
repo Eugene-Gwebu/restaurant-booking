10e33600e40bd4b052a84f1c1074e253f8cf6f20
Eugene Gwebu <gwebueugene@gmail.com>
2024-04-14 14:07:03 +0000

    added approval field to Booking model

--- booking_system/models.py
+++ booking_system/models.py
@@ -19,6 +19,7 @@ class Booking(models.Model):
     time = models.TimeField()
     cancel = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now=True)
+    approval = models.BooleanField()
 
     def __str__(self):
         return f"You have received a booking from {self.first_name} {self.last_name} ({self.email}) | on {self.date} | at {self.time}."
