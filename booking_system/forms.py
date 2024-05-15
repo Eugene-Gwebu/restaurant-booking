bc8d65c4ebaed1b27c0c525d881f25a7060dcf6b
Eugene Gwebu <gwebueugene@gmail.com>
2024-05-15 12:07:04 +0000

    updated date attribute in forms, views, and added template to confirm booking cancelation

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -14,7 +14,7 @@ class ConfirmBookingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['date'].widget = forms.widgets.DateInput(
-            attrs={'type': 'date'})
+            attrs={'type': 'date', 'min': datetime.now().strftime("%Y-%m-%d")})
         self.fields['time'].widget = forms.widgets.TimeInput(
             attrs={'type': 'time'})
 
@@ -35,23 +35,23 @@ class ConfirmBookingForm(forms.ModelForm):
         return cleaned_data
 
 
-    def save(self, commit=True):
-        """
-        Save method to edit booking information.
-        """
-        edit_booking = super(ConfirmBookingForm, self).save(commit=False)
+    # def save(self, commit=True):
+    #     """
+    #     Save method to edit booking information.
+    #     """
+    #     edit_booking = super(ConfirmBookingForm, self).save(commit=False)
         
-        booking = edit_booking
-        booking.first_name = self.cleaned_data['first_name']
-        booking.last_name = self.cleaned_data['last_name']
-        booking.email = self.cleaned_data['email']
-        booking.phone_number = self.cleaned_data['phone_number']
-        booking.places = self.cleaned_data['places'] 
-        booking.date = self.cleaned_data['date'] 
-        booking.time = self.cleaned_data['time']
+    #     booking = edit_booking
+    #     booking.first_name = self.cleaned_data['first_name']
+    #     booking.last_name = self.cleaned_data['last_name']
+    #     booking.email = self.cleaned_data['email']
+    #     booking.phone_number = self.cleaned_data['phone_number']
+    #     booking.places = self.cleaned_data['places'] 
+    #     booking.date = self.cleaned_data['date'] 
+    #     booking.time = self.cleaned_data['time']
 
-        if commit:
-            booking.save()
-            edit_booking.save()
-        return edit_booking
+    #     if commit:
+    #         booking.save()
+    #         edit_booking.save()
+    #     return edit_booking
        
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
-from django.views import generic 
 from django.contrib import messages
-from django.http import HttpResponseRedirect
-from .models import Booking
-from django.core.mail import send_mail
-from .forms import ConfirmBookingForm
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView 
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.views import generic
+from django.views.generic.edit import UpdateView
 
-
-
+from .forms import ConfirmBookingForm
+from .models import Booking
 
 # Create your views here.
 
 
 def home(request):
 
-     return render(request, 'booking_system/home.html')
+    return render(request, "booking_system/home.html")
 
 
 @login_required
@@ -26,11 +24,14 @@ def booking_form(request):
         confirm_booking_form = ConfirmBookingForm(data=request.POST)
         if confirm_booking_form.is_valid():
             booking = confirm_booking_form.save(commit=False)
-            booking.guest =request.user
+            booking.guest = request.user
             booking.save()
 
-            messages.success(request, 'Your booking was submitted successfully, and will be approved shortly!')  
-    
+            messages.success(
+                request,
+                "Your booking was submitted successfully, and will be approved shortly!",
+            )
+
     confirm_booking_form = ConfirmBookingForm()
 
     return render(
@@ -40,21 +41,24 @@ def booking_form(request):
             "confirm_booking_form": confirm_booking_form,
         },
     )
-     
+
 
 @login_required
 def confirmed_booking(request):
     """
     View to display booking details
     """
-    bookings = Booking.objects.filter(approval=True, guest = request.user)
+    bookings = Booking.objects.filter(approval=True, guest=request.user)
     guest = request.user
 
-    return render(request, 'booking_system/reservation.html', 
-    {
-        'guest': guest,
-        'bookings': bookings,
-    })
+    return render(
+        request,
+        "booking_system/reservation.html",
+        {
+            "guest": guest,
+            "bookings": bookings,
+        },
+    )
 
 
 @login_required
@@ -63,16 +67,27 @@ def edit_booking(request, booking_id):
     View to edit a booking
     """
     booking = get_object_or_404(Booking, pk=booking_id)
-    form = ConfirmBookingForm(data=request.POST, instance = booking)
-    
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect(reverse("confirmed_booking"))
+    form = ConfirmBookingForm(instance=booking)
 
-    context = {
-        "form": form
-    }
-    
+    if booking.guest != request.user:
+        messages.add_message(
+            request, messages.ERROR, "You cannot edit this booking!"
+        )
+        return redirect(reverse("homepage"))
+
+    if request.method == "POST":
+        form = ConfirmBookingForm(data=request.POST, instance=booking)
+        if form.is_valid():
+            updated_booking = form.save(commit=False)
+            updated_booking.approval = False
+            updated_booking.save()
+            messages.add_message(
+            request, messages.SUCCESS, "Your Booking was successfully Updated!"
+        )
+            return HttpResponseRedirect(reverse("confirmed_booking"))
+
+    context = {"form": form}
+
     return render(request, "booking_system/edit_booking.html", context)
 
 
@@ -83,13 +98,18 @@ def cancel_booking(request, booking_id):
     """
     booking = get_object_or_404(Booking, pk=booking_id)
 
+    if booking.guest != request.user:
+        messages.add_message(request, message.ERROR, "Booking not found!")
+        return redirect(reverse("homepage"))
+
     if request.method == "POST":
-        
-        booking.guest == request.user
         booking.delete()
-        messages.add_message(request, messages.SUCCESS, 'Your Booking was successfully deleted!')
-    else:
-        messages.add_message(request, messages.ERROR, 'Apologies, you cannot cancel this booking at the moment!')
-    
-    return HttpResponseRedirect(reverse('homepage'))
+        messages.add_message(
+            request, messages.SUCCESS, "Your Booking was successfully deleted!"
+        )
+        return redirect(reverse("homepage"))
+
+    return render(
+        request, "booking_system/cancel_confirmation.html", {"booking": booking}
+    )
 
