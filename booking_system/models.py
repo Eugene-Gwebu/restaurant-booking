3360de4692007ab595f9c05fc1eb0975496f4c63
Mbongeni Eugene Gwebu <gwebueugene@gmail.com>
2024-10-10 10:57:29 +0000

    Styled the navabr, and navbar icon on small screens

--- booking_system/models.py
+++ booking_system/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import IntegerField, Model
 from django.core.validators import MaxValueValidator, MinValueValidator
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -21,11 +22,11 @@ class Booking(models.Model):
     email = models.EmailField(default='example@example.com', max_length=200)
     phone_number = models.CharField(max_length=40, blank=True, null=True)
     places = models.PositiveIntegerField(
-        default=1, 
+        default=1,
         validators=[
-            MaxValueValidator(10), 
+            MaxValueValidator(10),
             MinValueValidator(1)])
-    date = models.DateField() 
+    date = models.DateField()
     time = models.TimeField()
     cancel = models.BooleanField(default=False)
     updated_on = models.DateTimeField(auto_now=True)
@@ -38,7 +39,3 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"You have received a booking from {self.first_name} {self.last_name} ({self.email}) | on {self.date} | at {self.time}."
-
-
-
-
--- the_lunchbar/settings.py
+++ the_lunchbar/settings.py
@@ -27,9 +27,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = [ '8000-eugenegwebu-restaurantb-3tht2rsvjmc.ws-eu111.gitpod.io',
+ALLOWED_HOSTS = [ '8000-eugenegwebu-restaurantb-81qv5lukc1v.ws.codeinstitute-ide.net',
 'pp4-restaurant-booking-f8fec4683622.herokuapp.com']
 
 
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -109,7 +111,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io/",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "https://8000-eugenegwebu-restaurantb-81qv5lukc1v.ws.codeinstitute-ide.net"
 ]
 
 # Password validation
@@ -148,8 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
